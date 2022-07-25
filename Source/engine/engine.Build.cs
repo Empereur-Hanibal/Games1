077a4c5638c3b6208415dc04cb78e45f5141054f
Empereur-Hanibal <78930962+Empereur-Hanibal@users.noreply.github.com>
2022-07-25 14:19:19 +0200

    first engine's commit

---
+++ Source/engine/engine.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class engine : ModuleRules
+{
+	public engine(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/engine/engineGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "engineGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AengineGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AengineGameMode();
+};
+
+
+
